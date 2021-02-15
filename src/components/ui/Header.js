abfd2977339b4088e06c28ae46fb7877f54a2c68
danielofaustino <danieloliveirafaustino@gmail.com>
2021-02-15 15:53:42 -0300

    Active tab refresh Fix

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,16 +1,16 @@
-import React, {useState } from 'react';
+import React, {  useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Button from '@material-ui/core/Button'
-import { Link } from "react-router-dom"
+import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
 
 
 //importing logo
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logo.png';
 
 // function NavBar Elevation 
 function ElevationScroll(props) {
@@ -73,6 +73,22 @@ export default function Header(props) {
   const handleChange = (e, value) => {
     setValue(value)
   }
+
+  useEffect(() =>{
+    if(window.location.pathname === "/" && value !== 0){
+      setValue(0)
+    } else if(window.location.pathname === "/services" && value !== 1){
+      setValue(1)
+    } else if(window.location.pathname === "/revolution" && value !== 2){
+      setValue(2)
+    } else if(window.location.pathname === "/about" && value !== 3){
+      setValue(3)
+    } else if(window.location.pathname === "/contact" && value !== 4){
+      setValue(4)
+    } else if(window.location.pathname === "/estimate" && value !== 5){
+      setValue(5)
+    } 
+  },[value]);
 
   
 
